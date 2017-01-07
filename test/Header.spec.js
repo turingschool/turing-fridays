69150189b016fb34468fce72359fde0b235b94cb
swanie21 <kswanie21@gmail.com>
2017-01-07 14:40:29 -0700

    test Spike component

---
+++ test/Header.spec.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { expect, assert } from 'chai';
+// import { shallow, mount } from 'enzyme';
+// import App from '../lib/components/App';
+// import Header from '../lib/components/Header';
+// import mockUsers from './helpers/mockUsers';
+//
+// describe('Header component', () => {
+//   const wrapper = shallow(<Header />);
+//
+//   context('mock user sign in rendering', () => {
+//
+//     const user = mockUsers[2];
+//     wrapper.setState({ user: user });
+//
+//     it('should be rendered into a <section> container', () => {
+//       assert.equal(wrapper.type(), 'section');
+//     });
+//   });
+// });
---
+++ test/Spikes.spec.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { expect, assert } from 'chai';
+// import { shallow, mount } from 'enzyme';
+// import Spikes from '../lib/components/Spikes';
+//
+// describe('Spikes component', () => {
+//   const wrapper = shallow(<Spikes />);
+//
+//   it('Spikes component should exist', () => {
+//     expect(wrapper).to.exist;
+//   });
+//
+//   it('should be rendered into a <section> container', () => {
+//     assert.equal(wrapper.type(), 'section');
+//   });
+//
+//   it('should have a dateFilter state', () => {
+//     expect(wrapper.state().dateFilter).to.equal('all');
+//   });
+// });
