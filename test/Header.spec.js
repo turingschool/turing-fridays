d346f29b299b4f31d877733c51e0aa0983aaa42e
Casey Cross <hellocaseycross@gmail.com>
2017-01-07 21:13:38 -0700

    Add mockSpikes file, move dateOptions function out of app and into Admin and Spikes components so it functions properly for testing.

--- test/Header.spec.js
+++ test/Header.spec.js
@@ -1,20 +1,22 @@
-// import React from 'react';
-// import { expect, assert } from 'chai';
-// import { shallow, mount } from 'enzyme';
-// import App from '../lib/components/App';
-// import Header from '../lib/components/Header';
-// import mockUsers from './helpers/mockUsers';
-//
-// describe('Header component', () => {
-//   const wrapper = shallow(<Header />);
-//
-//   context('mock user sign in rendering', () => {
-//
-//     const user = mockUsers[2];
-//     wrapper.setState({ user: user });
-//
-//     it('should be rendered into a <section> container', () => {
-//       assert.equal(wrapper.type(), 'section');
-//     });
-//   });
-// });
+import React from 'react';
+import { expect, assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import App from '../lib/components/App';
+import Header from '../lib/components/Header';
+import mockUsers from './helpers/mockUsers';
+
+describe('Header component', () => {
+  const wrapper = shallow(
+  <Header
+    user={ mockUsers[2] }
+    createSpike={(e) => this.createSpike(e)}
+    toggleForm={() => this.toggleForm()}
+  />);
+
+  context('mock user sign in rendering', () => {
+
+    it('should be rendered into a <header> container', () => {
+      assert.equal(wrapper.type(), 'header');
+    });
+  });
+});
