f08d351253a0693ce3d69848097cad67969411a8
swanie21 <kswanie21@gmail.com>
2017-01-07 23:12:25 -0700

    test Spikes and Header components

--- test/helpers/mockSpikes.js
+++ test/helpers/mockSpikes.js
@@ -1,7 +1,7 @@
 const mockSpikes = [
   {
-    key: "-KS3GtKnWB_XZKjgb0iJ",
-    title: "Graduating",
+    key: '-KS3GtKnWB_XZKjgb0iJ',
+    title: 'Graduating',
     description: 'Make that money!',
     createdAt: 1474319324550,
     createdBy:  'hellocaseycross@gmail.com',
@@ -10,14 +10,14 @@ const mockSpikes = [
     location: 'Classroom A',
     spikeDate: 1483315200000,
     user: {
-        displayName: "Casey Cross",
-        email: "hellocaseycross@gmail.com",
-        uid: "oSMIlUTN5OZOVY9pYfEi1PXCv9q1"
+        displayName: 'Casey Cross',
+        email: 'hellocaseycross@gmail.com',
+        uid: 'oSMIlUTN5OZOVY9pYfEi1PXCv9q1'
     }
   },
   {
-    key: "-KS3GtKnWB_XZKjgb0iJ",
-    title: "Being awesome",
+    key: '-KS3GtKnWB_XZKjgb0iJ',
+    title: 'Being awesome',
     description: 'Because I\'m Andy!',
     createdAt: 1474314439371,
     createdBy:  'atyoungdesign@gmail.com',
@@ -26,14 +26,14 @@ const mockSpikes = [
     location: 'Classroom A',
     spikeDate: 1483315289400,
     user: {
-        displayName: "Andy Young",
-        email: "atyoungdesign@gmail.com",
-        uid: "aL0MeDz7oXRqtSBPRYguthVf5aw2"
+        displayName: 'Andy Young',
+        email: 'atyoungdesign@gmail.com',
+        uid: 'aL0MeDz7oXRqtSBPRYguthVf5aw2'
     }
   },
   {
-    key: "-KS3GtKnWB_XZKjgb0iJ",
-    title: "Motorbike Madness",
+    key: '-KS3GtKnWB_XZKjgb0iJ',
+    title: 'Motorbike Madness',
     description: 'Put on your leather jacket and let\'s ride!',
     createdAt: 1474319324550,
     createdBy:  'kswanie21@gmail.com',
@@ -43,8 +43,8 @@ const mockSpikes = [
     spikeDate: 1483315200000,
     user: {
         displayName: 'Kirsten Swanson',
-        email: "kswanie21@gmail.com",
-        uid: "8opUzbSXdxOJcKgukMF4oIMPTu12"
+        email: 'kswanie21@gmail.com',
+        uid: '8opUzbSXdxOJcKgukMF4oIMPTu12'
     }
   }
 ];
